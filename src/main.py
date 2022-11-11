21ea8b10b5d58d5c7fe11b3f6f202f756cbc190c
amauryval <amauryval@gmail.com>
2022-11-11 12:53:18 +0100

    upd (*) c template

--- src/main.py
+++ src/main.py
@@ -1,11 +1,27 @@
 from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams.c4 import Person, Container, Database, System, SystemBoundary, Relationship
 
-with Diagram("Portfolio", outformat="png", show=False, direction="TB"):
-    ELB("lb") >> [EC2("worker1"),
-                  EC2("worker2"),
-                  EC2("worker3"),
-                  EC2("worker4"),
-                  EC2("worker5")] >> RDS("events")
+
+graph_attr = {
+    "splines": "spline",
+}
+
+with Diagram("Portfolio", direction="TB", graph_attr=graph_attr, show=False):
+    
+    developer = Person(
+        name="Developer", description="Update portfolio data on Github"
+    )
+    github_gist_input = System(name="Github Gist storage", description="Portfolio input data", external=True)
+
+    preprocessing = Container(
+        name="PreProcessing",
+        technology="Python - Pandas/GeoPandas, SqlAlchemy/GeoAlchmey",
+        description="Fill and/or update portfolio database",
+    )
+    github_ation_portfolio = System(name="Github Gist storage", description="Portfolio input data", external=True)
+
+    #with SystemBoundary("PreProcessing"):
+
+
+    developer >> Relationship("Update manually") >> github_gist_input
+    developer >> Relationship("event send") >> preprocessing >> github_ation_portfolio
